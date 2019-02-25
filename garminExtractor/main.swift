7751d2a60eaa5d8ed7d248215777cbb4a7d73246
Romain Quidet <romain.quidet.ext@mappy.com>
2019-02-25 13:39:05 +0100

    Initial Commit

---
+++ garminExtractor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  garminExtractor
+//
+//  Created by mappy on 25/02/2019.
+//  Copyright © 2019 mappy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
